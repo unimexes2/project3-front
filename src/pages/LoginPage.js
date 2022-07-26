f29bd8686a54b4719141b87a75686c0471fd272d
unimexes2 <unimexes2@gmail.com>
2022-07-26 11:33:27 +0200

    login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -31,22 +31,52 @@ function LoginPage(props) {
 	};
 
 	return (
-		<div className="LoginPage">
-			<h1>Login</h1>
+		
+		
+		
+		
+		
+		<div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <h2 className="text-center text-dark mt-5">Login Form</h2>
+        <div className="text-center mb-5 text-dark">Made with bootstrap</div>
+        <div className="card my-5">
 
-			<form onSubmit={handleLoginSubmit}>
-				<label>Email:</label>
-				<input type="text" name="email" value={email} onChange={handleEmail} />
+          <form className="card-body cardbody-color p-lg-5" onSubmit={handleLoginSubmit}>
 
-				<label>Password:</label>
-				<input type="password" name="password" value={password} onChange={handlePassword} />
+            <div className="text-center">
+              <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile"/>
+            </div>
 
-				<button type="submit">Login</button>
-			</form>
-			{errorMessage && <p className="error-message">{errorMessage}</p>}
+            <div className="mb-3">
+              <input type="text" className="form-control" id="Username" aria-describedby="emailHelp" value={email} onChange={handleEmail}
+                placeholder="User Name"/>
+            </div>
+            <div className="mb-3">
+              <input type="password" className="form-control" id="password" placeholder="password"type="password" value={password} onChange={handlePassword} />
+            </div>
+            <div className="text-center"><button type="submit" className="btn btn-color px-5 mb-5 w-100">Login</button></div>
+            <div id="emailHelp" className="form-text text-center mb-5 text-dark">Not
+              Registered? <a href="#" className="text-dark fw-bold"> Create an
+                Account</a>
+            </div>
+          </form>
+        </div>
 
-			<p>Don't have an account yet?</p>
-			<Link to={'/signup'}> Sign Up</Link>
+      </div>
+    </div>
+
+
+		
+		
+		
+
+		
+			{errorMessage && <p classNameName="error-message">{errorMessage}</p>}
+
+		
 		</div>
 	);
 }
