ac935ff97a0714a1947a6711d735e9b718be602e
unimexes2 <unimexes2@gmail.com>
2022-07-26 23:02:14 +0200

    hecho navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-
 import { useContext } from "react";                       // <== IMPORT 
 import { AuthContext } from "./../context/auth.context";  // <== IMPORT
 import Container from 'react-bootstrap/Container';
@@ -6,6 +5,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "./../pictures/logosite.png"
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function MyNavbar() {
   // Subscribe to the AuthContext to gain access to
   // the values from AuthContext.Provider `value` prop
@@ -13,7 +14,7 @@ function MyNavbar() {
 console.log(isLoggedIn)
   return (
   
-    
+    <div>
     
       <Navbar className="navbar">
       
@@ -21,36 +22,32 @@ console.log(isLoggedIn)
 
      
         <Nav className="me-auto">
-           
-           
-           
-          {isLoggedIn
-        ? (<>
-          
-            <Nav.Link className="menulinkadmin" onClick={logOutUser}>LOGOUT {user.name}</Nav.Link>
-           
-             <Nav.Link href="/add" className="menulinkadmin">
-              ADD ANIMAL
-            </Nav.Link>
-            <Nav.Link href="/addmap" className="menulinkadmin">
-              ADD MAP
-            </Nav.Link>
-            
-            <Nav.Link href="/addstories" className="menulinkadmin">
-              ADD STORY
-            </Nav.Link>
-
-          </>)
-        : 
-        (<></>)
-      }
+        
            
            
             <Nav.Link href="/cats" className="menulink">GATOS</Nav.Link>
 
             <Nav.Link href="/dogs" className="menulink">PERROS</Nav.Link>
             <Nav.Link href="/stories" className="menulink">HISTORIAS</Nav.Link>
-            <Nav.Link href="/dogadopted" className="menulink">PERROS ADOPTADOS</Nav.Link>
+
+
+
+            <NavDropdown className="menulink" title="ADOPTADOS" id="basic-nav-dropdown">
+              <NavDropdown.Item className="menulink"href="/dogadopted" >PERROS</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item className="menulink" href="#action/3.3">GATOS</NavDropdown.Item>
+            </NavDropdown>
+
+
+
+
+
+
+
+
+
+           
+           
             <Nav.Link href="/donate" className="menulink">DONACIONES</Nav.Link>
             <Nav.Link href="/map" className="menulink"> UBICACION </Nav.Link>
             <Nav.Link href="/login" className="menulink"> CONTACTOS </Nav.Link>
@@ -58,7 +55,46 @@ console.log(isLoggedIn)
     
       </Navbar>
 
-        
+      {isLoggedIn
+        ? (<>
+
+<Navbar className="w3-bar w3-blue" style={{fontFamily:"sans-serif", marginBottom:"10px"}}>
+
+   
+           
+           
+
+          
+            <Nav.Link className="w3-bar-item w3-button" onClick={logOutUser}>LOGOUT {user.name}</Nav.Link>
+           
+             <Nav.Link href="/add" className="w3-bar-item w3-button">
+              ADD ANIMAL
+            </Nav.Link>
+            <Nav.Link href="/addmap" className="w3-bar-item w3-button">
+              ADD MAP
+            </Nav.Link>
+            
+            <Nav.Link href="/addstories" className="w3-bar-item w3-button">
+              ADD STORY
+            </Nav.Link>
+            </Navbar>
+
+
+          </>)
+        : 
+        (<></>)
+      }
+
+
+
+
+
+
+
+
+
+
+      </div>        
       
      
 
