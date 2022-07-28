4972f957db84f3720bdb6a8a27fda7da28ae80bc
Sebastian Laca <seba.laca@gmail.com>
2022-07-28 11:10:31 +0200

    28/07 primer commit

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,39 +1,46 @@
-import CarouselBasement  from "../components/carousel";
+import CarouselBasement from "../components/carousel";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import protectlogo from "../pictures/protectora.png"
+import protectlogo from "../pictures/protectora.png";
 function Map() {
   const [mapId, setMap] = useState(null);
-  const [desc, setDesc]= useState(null);
+  const [desc, setDesc] = useState(null);
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/map")
-      .then((response) => {
-debugger
-        console.log(response.data[0].mapCode,"response")
-        setDesc(response.data[0].description);
-        setMap(response.data[0].mapCode);
-        //
-      });
+    axios.get("http://localhost:3000/map").then((response) => {
+      debugger;
+      console.log(response.data[0].mapCode, "response");
+      setDesc(response.data[0].description);
+      setMap(response.data[0].mapCode);
+      //
+    });
   }, []);
 
-    return (
-      
-      <div className="main-container">
-   <p>  {desc}           </p>
-   
-   {mapId?<iframe style={{width:600, height:400}}src={"https://www.google.com/"+mapId+ 'width="640" height="480"'}></iframe>:<></>
-
-    }
-
-
-       
-       
-  
-  
+  return (
+    <div className="main-container">
+      <div class="row w-150" >
+        <div  class="col-lg-6 my-4">
+          {mapId ? (
+            <iframe
+              src={
+                "https://www.google.com/" + mapId + 'width="640" height="480"'
+              }
+              class="w-100"
+              height="400"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div class="col-lg-6 my-4 d-flex align-items-center">
+          <div className="textDescription">
+            <p>{desc}</p>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Map;
-  
+    </div>
+  );
+}
+
+export default Map;
